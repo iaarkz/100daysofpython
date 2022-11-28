877bcdda2bd2877ccc9ec0f9673f7095b01a543d
Larissa Pinheiro <larissavspinheiro@gmail.com>
2022-11-28 11:15:29 -0300

    Day 001/100 - Working with variables in python to manage data

---
+++ day 1/main.py
@@ -0,0 +1,5 @@
+print("Welcome to the band name generator")
+city_name: str = input("What's the name of the city you grew up in?\n")
+pet_name: str = input("What's your pet name?\n")
+
+print("Your band name could be " + city_name + " " + pet_name)
